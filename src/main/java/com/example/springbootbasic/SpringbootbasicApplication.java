fff5446c74df009038c5ab5668adcb72fbb75f47
YooooSW <pk8652@naver.com>
2024-09-26 14:14:36 +0900

    springsecurity

---
+++ src/main/java/com/example/springbootbasic/SpringbootbasicApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicApplication.class, args);
+	}
+}
